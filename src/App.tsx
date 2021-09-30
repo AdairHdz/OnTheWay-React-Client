8f48935584d41f6181a038965ffe78049e2071c2
AdairHdz <adairho16@gmail.com>
2021-09-30 10:47:26 -0500

    chore: The initial structure for the project has been created

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,  
+} from "react-router-dom";
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Switch>
+          <Route path="/" exact>
+            <p className="text-red-500">Hello world</p>    
+          </Route>
+          <Route path="/home">
+            <p className="text-blue-500">Home</p>
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
